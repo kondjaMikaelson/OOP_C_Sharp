93121b02a7b2acfd1df8148e10c1dd9cc1f07762
Alexander <52912402+AlexanderNdeumane@users.noreply.github.com>
2019-09-02 14:03:26 +0200

    Add files via upload

---
+++ AssociationsLibrary/AssociationsLibrary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssociationsLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create and initialize record object
+            Record rec = new Record();
+            //add books to record
+            rec.AddBook(new Book("Harry Potter",9292));
+            rec.AddBook(new Book("Diary of A Wimpy Kid", 9993));
+            rec.AddBook(new Book("Soccer Bosses", 9532));
+            rec.AddBook(new Book("Java Tutorials", 9789));
+            rec.AddBook(new Book("How to Code", 9362));
+            rec.AddBook(new Book("Black Panther", 9454));
+            //adds student to record
+            rec.AddStudent(new Student("Alexander",201707042));
+            //checks if harry potter book is available
+            Console.WriteLine(rec.IsAvailable(9292));
+            //borrows harry potter, Diary of A Wimpy Kid, Soccer Bosses
+            //and Java Tutorials to Alexander
+            rec.BorrowBook("Alexander", 201707042, 9292, "06/08/2019", "14/08/2019");
+            rec.BorrowBook("Alexander", 201707042, 9993, "06/08/2019", "14/08/2019");
+            rec.BorrowBook("Alexander", 201707042, 9532, "05/06/2019", "12/06/2019");
+            rec.BorrowBook("Alexander", 201707042, 9789, "04/05/2019", "16/05/2019");
+            //checks if harry potter is available
+            Console.WriteLine(rec.IsAvailable(9292));
+            //checks if black panther is available
+            Console.WriteLine(rec.IsAvailable(9454));
+            //checks how many books are to be returned by 14/08/2019
+            Console.WriteLine(rec.TotalToBeReturnedBy("14/08/2019"));
+        }
+    }
+}
---
+++ AssociationsLibrary/AssociationsLibrary/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssociationsLibrary
+{
+    class Student
+    {
+        //attributes for student class
+        private string name;
+        private int studentNumber;
+        private Book[] books;
+        private int count;
+        //constructor for student classs. Takes in student
+        //name and student number
+        public Student(string name, int studentNumber)
+        {
+            this.name = name;
+            this.studentNumber = studentNumber;
+            books = new Book[1000];
+            count = 0;
+        }
+        //method used to assign a book to a student. Takes in a
+        //book object, borrow date and return date as its parameters.
+        //Books are assigned to the array of "books"
+        public void AssignBook(Book book, string borrowDate, string returnDate)
+        {
+            books[count] = book;
+            books[count].Borrow(borrowDate, returnDate);
+            count++;
+        }
+        //get method that returns student number
+        public int GetStudentNumber()
+        {
+            return studentNumber;
+        }
+        //get method that returns student name
+        public string GetStudentName()
+        {
+            return name;
+        }
+        
+    }
+}
